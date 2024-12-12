f6f68eac4eed7a994c2760f4fb5bf652fdb228af
Mathis934 <m.semery@lprs.fr>
2024-12-12 14:26:15 +0100

    feat(Choix du noms des joueurs et initialisation du Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.print ("Entrez le nom du joueur 1 : ");
+        String player1 = clavier.nextLine();
+        System.out.print("Entrez le nom du joueur 2 : ");
+        String player2 = clavier.nextLine();
     }
 }
+
